d83ca1e6d3393322d3dcc54a8f169314458d888b
Reetpriye <reetpriye@gmail.com>
2020-07-20 12:13:43 +0530

    Add More Programs

---
+++ 36InheritanceAndDifferentTypes.cpp
@@ -0,0 +1,59 @@
+// Inheritance & it's Different Types
+// Author: REET
+
+/*
+============WHY INHERITANCE?============
+Reusability is a very important concept 
+in Object Oriented Programming.
+In C++, we can reuse a class and add
+additional features to it.
+Reusing classes saves time and money.
+Reusing already tested and debugged class
+will save a lot of effort of developing
+and debugging the same thing again.
+The concept of reusability is supported
+using inheritance.
+We can reuse the properties of an
+existing class by inheriting from it.
+The existing class is called Base Class.
+The new class which is inherited is
+called as the Derived Class.
+==========TYPES OF INHERITANCE==========
+1. Single Inheritance
+2. Multiple Inheritance
+3. Hierarchical Inheritance
+4. Multilevel Inheritance
+5. Hybrid Inheritance
+----------------------------------------
+# Single Inheritance: A derived class
+with only one base class.
+Example: A -> B
+# Multiple Inheritance: A derived class 
+with more than one base class.
+class.
+Example: A,B -> B
+# Hierarchical Inheritance: Several
+derived classes from one base class.
+Example: A -> A,B
+# Multilevel Inheritance: Deriving a
+class from already derived class.
+Example: A -> B -> C
+# Hybrid Inheritance: It is a combination
+of multiple inheritance and multilevel
+inheritance.
+A class is derived from two classes as in
+multiple inheritance.
+However, one of the parent classes is not
+a base class.
+Example: A -> B,C -> D
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    
+
+  return 0;
+}
